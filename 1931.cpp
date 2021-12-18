7659cabdb52fdd534d9f89cf371beda38c65d248
MyunghyungNero <mh1kim0910@gmail.com>
2021-12-18 23:08:54 +0900

    1931

--- 1931.cpp
+++ 1931.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-bool cmp(const pair<int,int> &p,const pair<int,int> &p2){
+bool cmp(const pair<int,int> &p,const pair<int,int> &p2){    //회의 마감시간 기준으로 정렬
     if(p.second==p2.second)
         return p.first < p2.first;
     return p.second<p2.second;
 }
 int main(){
-    vector<pair<int,int>> v;
+    vector<pair<int,int>> v;       //회의 시간과 마감시간 저장
     int n;
     cin >> n;
     for(int i=0;i<n;i++){
@@ -20,7 +20,7 @@ int main(){
     int min =v[0].second;
     int cnt=1;
     for(int i=1;i<n;i++){
-        if(min<=v[i].first){
+        if(min<=v[i].first){ //회의 시간이 전 회의 마감보다 같거나 길어야함
             cnt++;
             min=v[i].second;
         }
