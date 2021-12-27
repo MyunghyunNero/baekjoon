232cd8fbbd0e07b4a7b0cbbca55b8e9c684e19f5
MyunghyungNero <mh1kim0910@gmail.com>
2021-12-27 16:19:18 +0900

    2875

--- 2875.cpp
+++ 2875.cpp
@@ -4,7 +4,7 @@ int main(){
     int m,n,k;
     cin >> m>>n>>k;
     int ans=0;
-    while(m+n>=k+3 && m>=2 && n>=1){
+    while(m+n>=k+3 && m>=2 && n>=1){   //세가지 조건 만족시키면서 반복
         ans++;
         m-=2;
         n-=1;
