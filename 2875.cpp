56f0b36dbee9930152b3838561adf6677307757a
MyunghyungNero <mh1kim0910@gmail.com>
2021-12-27 16:16:24 +0900

    2875

---
+++ 2875.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int m,n,k;
+    cin >> m>>n>>k;
+    int ans=0;
+    while(m+n>=k+3 && m>=2 && n>=1){
+        ans++;
+        m-=2;
+        n-=1;
+    }
+    cout << ans;
+}
