5e3a5408206c942574b45c38e8fddefa713c4cd5
MyunghyungNero <mh1kim0910@gmail.com>
2021-12-31 01:49:59 +0900

    11728

--- 11728.cpp
+++ 11728.cpp
@@ -35,6 +35,9 @@ void merge(int alen,int blen){
     }
 }
 int main(){
+    cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
     int n,m;
     cin >> n>>m;
     for(int i=0;i<n;i++){
