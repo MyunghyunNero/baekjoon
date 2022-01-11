f1a2f51deb78adb1442b2609a9db30c88286d4b5
MyunghyungNero <mh1kim0910@gmail.com>
2022-01-11 23:13:17 +0900

    10971

--- 10971.cpp
+++ 10971.cpp
@@ -8,7 +8,7 @@ int main(){
     vector<vector<int>> d(n);
     vector<int> ord(n);
     for(int i=0;i<n;i++){
-        ord[i]=i;
+        ord[i]=i;                 //순서 저장하는 배열
     }
     for(int i=0;i<n;i++){
         for(int k=0;k<n;k++){
@@ -22,18 +22,18 @@ int main(){
         bool ok=true;
         int sum=0;
         for(int i=0;i<n-1;i++){
-            if(d[ord[i]][ord[i+1]]==0){
+            if(d[ord[i]][ord[i+1]]==0){         // 값이 0이면 못가는 것이므로 false
                 ok=false;
             }
             else{sum+=d[ord[i]][ord[i+1]];}
         }
-        if(ok&&d[ord[n-1]][ord[0]]!=0){
+        if(ok&&d[ord[n-1]][ord[0]]!=0){        //갈 수없는 배열은 더하지 말기
             sum+=d[ord[n-1]][ord[0]];
             if(ans>sum){
                 ans=sum;
             }
         }
-    }while(next_permutation(ord.begin(),ord.end()));
+    }while(next_permutation(ord.begin(),ord.end())); //배열 순서 다 확인하기
     cout << ans;
 
 }
---
+++ 6603.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    while(1){
+        int n;
+        cin >>n; 
+        if(n==0){            //탈출
+            break;
+        }
+        vector<int> a(n);      
+        for(int i=0;i<n;i++){           //집합 수
+            cin>>a[i];
+        }
+        vector<int> d;
+        for(int i=0;i<n-6;i++){        //0과 1(6개)을 넣어 순열 함수를 사용한다음 그 위치 값 그대로 원소 6개 집합 만들기
+            d.push_back(0);
+        }
+        for(int i=0;i<6;i++){
+            d.push_back(1);
+        }
+        vector<vector<int>> ans;
+        do{
+            vector<int> cur;
+            for(int i=0;i<n;i++){
+                if(d[i]==1){                     //1일때 위치값을 집합배열 그대로 넣기
+                    cur.push_back(a[i]);
+                }
+            }
+            ans.push_back(cur);
+        }while(next_permutation(d.begin(),d.end()));      //다음 순열
+        sort(ans.begin(),ans.end());          //정렬
+        for(auto &v :ans){
+            for(int i=0;i<v.size();i++){
+                cout <<v[i]<<' ';
+            }
+            cout << '\n';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
