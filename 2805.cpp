dff9762b03771ddc680362570a2dda75d694e408
MyunghyungNero <mh1kim0910@gmail.com>
2022-01-21 15:42:54 +0900

    2805

--- 2805.cpp
+++ 2805.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 int n,m;
-bool check(vector<long long>& v,int x){
+bool check(vector<long long>& v,int x){        //잘려진 나무의 총 길이와 가져가야하는 나무의 길이 비교
     long long cnt=0;
     for(int i=0;i<n;i++){
         long long temp= v[i]-x;
@@ -25,12 +25,12 @@ int main(){
     long long left=0;
     long long right=max;
     long long ans=0;
-    while(left<=right){
+    while(left<=right){     //가장 큰 길이를 가진 나무까지만 톱날을 자르는게 의미가 있으므로 right는 가장 큰
         long long mid=(left+right)/2;
         int ok=check(v,mid);
         if(ok){
             left=mid+1;
-            if(ans<mid){
+            if(ans<mid){          //높이 최대값을 구해야하므로 
                 ans=mid;
             }
         }
