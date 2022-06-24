f6ca48f93d430a3ddaa4d052add444aff19fa0d5
MyunghyungNero <mh1kim0910@gmail.com>
2022-06-25 00:53:47 +0900

    15651

---
+++ 2022.06/15651.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n,m;
+int arr[10];
+void dfs(int cnt){
+    if(cnt==m){
+        for(int i=0;i<m;i++){
+            cout<<arr[i]<<' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i=1;i<=n;i++){
+        arr[cnt]=i;
+        dfs(cnt+1);
+    }
+}
+int main(){
+    cin >>n>>m;
+    dfs(0);
+}
