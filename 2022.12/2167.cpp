574bac12fe585071448cdbee23d4f4680d1ff056
MyunghyunNero <mh1kim0910@gmail.com>
2022-12-13 16:44:26 +0900

    2167

---
+++ 2022.12/2167.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int n,m;
+int arr[302][302];
+int sun[302][302];
+int main(){
+    cin >> n>>m;
+    int accSum=0;
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=m;k++){
+            int a;
+            cin>>a;
+            arr[i][k]=a;
+            accSum+=(a+sum[i-1][k]+sum[i][k-1]-sum[i-1][k-1]);
+            sum[i][k]=accSum;
+        }
+    }
+
+    int t;
+    cin>>t;
+    while(t--){
+        int i,j,x,y;
+        cin>>i>>j>>x>>y;
+        cout << sum[x][y]-sum[i-1][y]-sum[x][j-1]+sum[i-1][j-1];
+    }
+}
