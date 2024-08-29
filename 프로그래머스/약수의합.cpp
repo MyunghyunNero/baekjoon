85506301bf817c5e4b565ab4cdf6eb13dd35d386
myunghyunnero <aimk12@naver.com>
2024-08-29 22:27:31 +0900

    프로그래머스 : 약수의 합

---
+++ 프로그래머스/약수의합.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int d[3001];
+int solution(int n) {
+    int answer = 0;
+    for(int i=1;i*i<=n;i++) {
+        if(n%i==0) {
+            if(i == n/i) {
+                answer +=i;
+            }else {
+                answer +=i;
+                answer += n/i;
+            }
+        }
+    }
+    return answer;
+}
